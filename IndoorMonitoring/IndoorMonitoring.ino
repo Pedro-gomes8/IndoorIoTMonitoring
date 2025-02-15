86656e0da35e46eaa326e135a64a217181d9692d
AndreTells <andresilvatelles@gmail.com>
2025-02-15 18:06:52 +0100

    finished measurements

--- IndoorMonitoring/IndoorMonitoring.ino
+++ IndoorMonitoring/IndoorMonitoring.ino
@@ -4,58 +4,50 @@
 #include <Wire.h>
 
 // ------- SENSORS -----
-#include <Adafruit_BMP280.h>
-#include <Adafruit_SGP30.h> // For TVOC, ECO2, H2 and Ethanol
-#include <Adafruit_Si7021.h> // For temperature and humidity Methods: readHumidity, readTemperature
+#include <SoftwareSerial.h>
 
-#define CS 10
+#include "hardware.h"
+#include "measurements.h"
+#include "sensors.h"
+
+#define SERIAL_BAUDRATE 115200
+#define BLUETOOTH_BAUDRATE 9600
 
 
-Adafruit_Si7021 si = Adafruit_Si7021();
-Adafruit_SGP30 sgp;
-Adafruit_BMP280 bmp;
+SoftwareSerial mySerial(9,8); // RX, TX
+String w;
+
+Sensors sensors;
 
 // ------- SD ------
 File myFile;
 
+void print_measurement(measurement_t* measure){
+  Serial.print("Humidity: ");
+  Serial.print(measure->humidity);
+  Serial.print(", Temperature: ");
+  Serial.print(measure->temperature);
+  Serial.print(", TVOC: ");
+  Serial.print(measure->TVOC);
+  Serial.print(", eCO2: ");
+  Serial.print(measure->eCO2);
+  Serial.print(", pressure: ");
+  Serial.println(measure->pressure);
+
+}
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(SERIAL_BAUDRATE);
+  mySerial.begin(BLUETOOTH_BAUDRATE);
   while (!Serial){; // Wait for serial port to connect
   }
 
   Serial.println("Initializing Sensors ----");
 
-  /* --------------------------------------
-  *           SENSORS
-  *. --------------------------------------
-  */
-  // --- SI7021
-  Serial.print("\tSi 7021: ");
-
-  if (!si.begin()){
-    Serial.println("Did not find Si7021 sensor!");
-    while (true) ;
+  if(!sensors.begin()){
+    Serial.println("Failed");
   }
-  Serial.println("OK");
-
-  // --- SGP30
-  Serial.print("\tSGP30: ");
-  if (!sgp.begin()){
-    Serial.println("Did not find SGP30 sensor!");
-    while (true) ;
-  }
-  Serial.println("OK");
-
-  // --- BMP280
-  Serial.print("\tBMP280: ");
-  if (!bmp.begin()){
-    Serial.println("Did not find BMP280 Sensor!");
-    while (true) ; 
-  }
-  Serial.println("OK");
-
 
   // --- SD CARD
   Serial.print("Initializing SD card: ");
@@ -68,13 +60,31 @@ void setup() {
 
   // bmp.setSampling();
 
-
-
-
-
+  pinMode(MY_LED_BUILTIN,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(MY_LED_BUILTIN, HIGH); // sets the digital pin 13 on
+  delay(1000);            // waits for a second
 
+  measurement_t measure;
+  sensors.measure(&measure);
+  print_measurement(&measure);
+
+
+  if (mySerial.available()) {
+    w = mySerial.readString();
+    Serial.println(w);  //PC
+    delay(10);
+    //commands with Serial.println(); show on pc serial monitor
+  }
+
+  mySerial.println("This is a test run");
+  mySerial.println("Congratulations! Device Connected!");
+  mySerial.println();
+
+
+  digitalWrite(MY_LED_BUILTIN, LOW);  // sets the digital pin 13 off
+  delay(2000);            // waits for a second
 }
---
+++ IndoorMonitoring/sensors.cpp
@@ -0,0 +1,50 @@
+#include "sensors.h"
+
+int Sensors::begin(){
+  this->si = Adafruit_Si7021();
+
+  Serial.print("\tSi 7021: ");
+
+  if (!this->si.begin()){
+    Serial.println("Did not find Si7021 sensor!");
+    return 0;
+  }
+  Serial.println("OK");
+
+  // --- SGP30
+  Serial.print("\tSGP30: ");
+
+  if (!this->sgp.begin()){
+    Serial.println("Did not find SGP30 sensor!");
+    return 0;
+  }
+  Serial.println("OK");
+
+  // --- BMP280
+  Serial.print("\tBMP280: ");
+
+  if (!this->bmp.begin()){
+    Serial.println("Did not find BMP280 Sensor!");
+    return 0;
+  }
+  Serial.println("OK");
+
+  return 1;
+}
+
+int Sensors::measure(measurement_t* data){
+  data->humidity = this->si.readHumidity();
+  data->temperature = this->si.readTemperature();
+
+  if (! this->sgp.IAQmeasure()) {
+    Serial.println("Measurement failed");
+    return -1;
+  }
+
+  data->TVOC = this->sgp.TVOC;
+  data->eCO2 = this->sgp.eCO2;
+
+  data->pressure = this->bmp.readPressure();
+
+  return 0;
+}
